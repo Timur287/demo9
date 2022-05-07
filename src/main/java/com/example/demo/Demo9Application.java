8e37cfd5be03d54f55e7f001af17e1a5d42bf122
Timur287 <tmuxametov@gmail.com>
2022-05-07 10:13:45 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/Demo9Application.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import com.example.demo.timur.entity.ThemeParkRide;
+import com.example.demo.timur.repository.ThemeParkRideRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Demo9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo9Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner sampleData(ThemeParkRideRepository repository) {
+        return (args) -> {
+            repository.save(new ThemeParkRide("Rollercoaster", "Train ride that speeds you along.", 5, 3));
+            repository.save(new ThemeParkRide("Log flume", "Boat ride with plenty of splashes.", 3, 2));
+            repository.save(new ThemeParkRide("Teacups", "Spinning ride in a giant tea-cup.", 2, 4));
+        };
+    }
+}
