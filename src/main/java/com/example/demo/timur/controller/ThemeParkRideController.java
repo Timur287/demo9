0689787cc33d2e38ea6297460052696048ad7488
Timur287 <tmuxametov@gmail.com>
2022-05-26 19:19:38 +0300

    remome dokerhub err5

--- src/main/java/com/example/demo/timur/controller/ThemeParkRideController.java
+++ src/main/java/com/example/demo/timur/controller/ThemeParkRideController.java
@@ -1,12 +1,16 @@
 package com.example.demo.timur.controller;
 
-import com.example.demo.timur.Exception.ThemeParkRideException;
+import com.example.demo.timur.controller.Exception.ErrorResponse;
+import com.example.demo.timur.controller.Exception.ThemeParkRideException;
 import com.example.demo.timur.entity.ThemeParkRide;
 import com.example.demo.timur.repository.ThemeParkRideRepository;
-import org.springframework.http.HttpStatus;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 
@@ -19,22 +23,69 @@ public class ThemeParkRideController {
         this.themeParkRideRepository = themeParkRideRepository;
     }
 
-    @GetMapping(value = "/ride", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Get a all rides")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "All rides",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ThemeParkRide.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content =@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "Ride not found",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)))
+    }) @GetMapping(value = "/ride", produces = MediaType.APPLICATION_JSON_VALUE)
     public Iterable<ThemeParkRide> getRides() {
         return themeParkRideRepository.findAll();
     }
 
-    @GetMapping(value = "/ride/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Get a ride by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the ride",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ThemeParkRide.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content =@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "Ride not found",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)))
+    }) @GetMapping(value = "/ride/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ThemeParkRide getRide(@PathVariable long id){
-        return themeParkRideRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Invalid ride id %s", id)));
+        return themeParkRideRepository.findById(id).orElseThrow(() -> new ThemeParkRideException( String.format("Invalid ride id %s", id)));
     }
-    @GetMapping(value = "/rideex/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+
+    @Operation(summary = "Get a ride by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the ride",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ThemeParkRide.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content =@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "Ride not found",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)))
+    }) @GetMapping(value = "/rideex/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ThemeParkRide getRideEx(@PathVariable long id){
         return themeParkRideRepository.findById(id).orElseThrow(() -> new ThemeParkRideException("hello world!"));
     }
 
-    @PostMapping(value = "/ride", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+
+    @Operation(summary = "Post ride")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Post the ride",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ThemeParkRide.class)) }),
+            @ApiResponse(responseCode = "500", description = "Something ride wrong",
+                    content =@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "Ride with simple id",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)))
+    }) @PostMapping(value = "/ride", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ThemeParkRide createRide(@Valid @RequestBody ThemeParkRide themeParkRide) {
         return themeParkRideRepository.save(themeParkRide);
     }
+
 }
